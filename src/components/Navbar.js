6229ec2df00800764cc125419d9ca427feb4d7bc
Vivemsury <129031932+Vivemsury@users.noreply.github.com>
2023-08-23 09:35:47 +0530

    i changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,8 @@ import React from 'react'
 
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-
+// import { useState } from 'react'
+// const [home,sethome]=useState
 
 export default function Navbar(props) {
     return (
